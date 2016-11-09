725930df9cca10a41e7612f9988b30c062fdc813
jipee37 <jipee37@yahoo.fr>
2016-11-09 17:29:25 +0100

    some javascript added

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	
+	// Display other languages on click
+  $("nav li.active a").on('click', function(e) {
+    e.preventDefault();
+    $('nav li:not(.active)').slideToggle(400);
+    // $(this).closest('li').siblings('li').slideToggle(400);
+  });
+});
